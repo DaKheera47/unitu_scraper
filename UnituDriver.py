775a99642e17cb378a7cc094280c14a631c0ea9f
DaKheera47 <shaheergoodboy@gmail.com>
2024-05-31 15:25:50 +0100

    issue opener and closer issues resolved & properties added

--- UnituDriver.py
+++ UnituDriver.py
@@ -111,75 +111,110 @@ class UnituDriver(webdriver.Edge):
         post_id = url.split('-')[-1]
 
         self.driver.get(url)
+        current_data["title"] = self.driver.find_element(By.ID, "feedbackTitle").text
         current_data["description"] = self.driver.find_element(By.ID, f"full_description_{post_id}").get_attribute(
             'innerHTML').strip()
-        current_data["title"] = self.driver.find_element(By.ID, "feedbackTitle").text
         current_data["upvotes"] = self.driver.find_element(By.ID, f"countPositive_{post_id}").text
         current_data["downvotes"] = self.driver.find_element(By.ID, f"countNegative_{post_id}").text
         current_data["timer"] = self.driver.find_element(By.ID, f"feedback-timer").text
 
-        current_data['Feedback Details'] = self.driver.find_element(By.XPATH,
+        current_data["feedback_details"] = self.driver.find_element(By.XPATH,
                                                                     "//h6[contains(text(),'Feedback Details')]").text
-        current_data['Type'] = self.driver.find_element(By.XPATH,
+        current_data["type"] = self.driver.find_element(By.XPATH,
                                                         "//div[contains(text(),'Type')]/following-sibling::div/span[@data-cy='feedback-type']").text
-        current_data['Status'] = self.driver.find_element(By.XPATH,
+        current_data["status"] = self.driver.find_element(By.XPATH,
                                                           "//div[contains(text(),'Status')]/following-sibling::div").text
-        current_data['Viewed'] = self.driver.find_element(By.XPATH,
+        current_data["viewed"] = self.driver.find_element(By.XPATH,
                                                           "//div[contains(text(),'Viewed')]/following-sibling::div").text
-        current_data['Feedback category'] = self.driver.find_element(By.XPATH,
+        current_data["feedback_category"] = self.driver.find_element(By.XPATH,
                                                                      "//div[contains(text(),'Feedback category')]/following-sibling::div").text
-        current_data['NSS category'] = self.driver.find_element(By.XPATH,
-                                                                "//div[contains(text(),'NSS category')]/following-sibling::div").text
 
         try:
-            current_data['Year'] = self.driver.find_element(
+            current_data['year'] = self.driver.find_element(
                 By.XPATH, "//div[contains(text(),'Year')]/following-sibling::div"
             ).text
         except NoSuchElementException:
-            current_data['Year'] = ""
+            current_data['year'] = ""
 
         try:
-            current_data['Module'] = self.driver.find_element(
+            current_data['module'] = self.driver.find_element(
                 By.XPATH, "//div[contains(text(),'Module')]/following-sibling::div"
             ).text
         except NoSuchElementException:
-            current_data['Module'] = ""
+            current_data['module'] = ""
 
         try:
-            current_data['Assignee'] = self.driver.find_element(
+            current_data['assignee'] = self.driver.find_element(
                 By.XPATH, "//div[contains(text(),'Assignee')]/following-sibling::div"
             ).text.split('\n')[1]
         except NoSuchElementException:
-            current_data['Assignee'] = ""
+            current_data['assignee'] = ""
 
         current_data['staff_views'] = self.driver.find_element(By.XPATH,
                                                                "//div[contains(text(),'Staff')]/following-sibling::div").text
         current_data['student_views'] = self.driver.find_element(By.XPATH,
                                                                  "//div[contains(text(),'Students')]/following-sibling::div").text
 
-        # Find the div containing the text "Changed an issue status to Open"
-        issue_status_div = self.driver.find_element(By.XPATH,
-                                                    "//div[contains(text(), 'Changed an issue status to Open')]")
-        # Navigate to the previous sibling to find the 'feedback-history-author' div
-        sibling_div = issue_status_div.find_element(By.XPATH, "./preceding-sibling::div")
-        current_data["issue_opener_name"] = sibling_div.find_element(By.CSS_SELECTOR, "span.h6").text
-        current_data["issue_opened_how_long_ago"] = sibling_div.find_elements(By.CSS_SELECTOR,
-                                                                             "span.small.text-dark-600")[0].text
-        current_data["issue_opener_designation"] = sibling_div.find_elements(By.CSS_SELECTOR,
-                                                                            "span.small.text-dark-600")[1].text
-        current_data["issue_opener_role"] = sibling_div.find_element(By.CSS_SELECTOR,
-                                                                     "span.badge").text
+        # -------------- open issues --------------
+        try:
+            issue_status_div = self.driver.find_element(By.XPATH, "//div[contains(text(), 'status to Open')]/..")
+
+            current_data["issue_opener_name"] = issue_status_div.find_element(By.CSS_SELECTOR, "span.h6").text
+            current_data["issue_opened_how_long_ago"] = issue_status_div.find_elements(By.CSS_SELECTOR,
+                                                                                       "span.small.text-dark-600")[
+                0].text
+            current_data["issue_opener_designation"] = issue_status_div.find_elements(By.CSS_SELECTOR,
+                                                                                      "span.small.text-dark-600")[
+                1].text
+            current_data["issue_opener_role"] = issue_status_div.find_element(By.CSS_SELECTOR, "span.badge").text
+        except NoSuchElementException as e:
+            print(f"unable to find all fields of open issues. Exception: {e}")
+
+        # -------------- closed issues --------------
+        if current_data["status"] == "Closed":
+            try:
+                divs_with_close = self.driver.find_elements(By.XPATH, "*//div[contains(text(), 'Closed')]/..")
+
+                if len(divs_with_close) <= 0:
+                    raise NoSuchElementException(msg="no divs with 'closed' in it")
+
+                issue_status_div = None
+                for div in divs_with_close:
+                    # Find the div that's also a feedback-history-container
+                    try:
+                        issue_status_div = div.find_element(By.XPATH, "..//*[@data-cy='feedback-history-container']")
+                    except NoSuchElementException:
+                        pass
+
+                if not issue_status_div:
+                    print("unable to find div with 'Closed' and @data-cy='feedback-history-container'")
+                    raise NoSuchElementException(msg="unable to find all fields of close issues")
+
+                current_data["issue_closer_name"] = issue_status_div.find_element(By.CSS_SELECTOR, "span.h6").text
+                current_data["issue_closed_how_long_ago"] = issue_status_div.find_elements(By.CSS_SELECTOR,
+                                                                                           "span.small.text-dark-600")[
+                    0].text
+                current_data["issue_closer_designation"] = issue_status_div.find_elements(By.CSS_SELECTOR,
+                                                                                          "span.small.text-dark-600")[
+                    1].text
+                current_data["issue_closer_role"] = issue_status_div.find_element(By.CSS_SELECTOR,
+                                                                                  "span.badge").text
+            except NoSuchElementException as e:
+                print(f"unable to find all fields of closed issues. Exception: {e}")
 
         current_data["url"] = url
 
         self.data.append(current_data)
 
-        print(f"{json.dumps(self.data, indent=4)}")
+        # print(f"{json.dumps(self.data, indent=4)}")
 
         # close this tab
         self.driver.close()
         # switch context to the first tab
         self.driver.switch_to.window(self.driver.window_handles[0])
+
+    def get_data(self):
+        return self.data
 
     def grab_posts(self):
         # open
@@ -194,20 +229,17 @@ class UnituDriver(webdriver.Edge):
         closed_div = self.driver.find_element(By.ID, "closed-drop-here")
         closed_tickets = closed_div.find_elements(By.CSS_SELECTOR, ".feedback-ticket")
 
-        # print("Open tickets: ")
-        # for ticket in open_tickets:
-        #     a_tag = ticket.find_element(By.TAG_NAME, "a")
-        #     self.scrape_post(a_tag.get_attribute("href"))
+        for ticket in open_tickets:
+            a_tag = ticket.find_element(By.TAG_NAME, "a")
+            self.scrape_post(a_tag.get_attribute("href"))
 
-        print("In Progress tickets: ")
         for ticket in in_progress_tickets:
             a_tag = ticket.find_element(By.TAG_NAME, "a")
             self.scrape_post(a_tag.get_attribute("href"))
-        #
-        # print("Closed tickets: ")
-        # for ticket in closed_tickets:
-        #     a_tag = ticket.find_element(By.TAG_NAME, "a")
-        #     self.scrape_post(a_tag.get_attribute("href"))
+
+        for ticket in closed_tickets:
+            a_tag = ticket.find_element(By.TAG_NAME, "a")
+            self.scrape_post(a_tag.get_attribute("href"))
 
     def is_logged_in(self):
         username = self.driver.find_element(By.CSS_SELECTOR, ".menu-username")
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from UnituDriver import UnituDriver
 
 if __name__ == '__main__':
     driver = UnituDriver(
-        headless=False
+        headless=True
     )
 
     driver.login()
 
     driver.grab_posts()
+
+    print(driver.get_data())
 
     # # store the html of the page to a file
     # with open("file.html", "w") as f:
