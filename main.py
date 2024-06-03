d2e878132d850e43893457f814483a4aa5e7b2d1
DaKheera47 <shaheergoodboy@gmail.com>
2024-06-03 15:44:29 +0100

    paralleling driver creation and running

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
+import json
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 from UnituDriver import UnituDriver
 
-if __name__ == '__main__':
-    driver = UnituDriver(
-        headless=True
-    )
 
+def process_board(urls):
+    driver = UnituDriver(headless=HEADLESS)
+    driver.login()
+
+    for idx, url in enumerate(urls, start=1):
+        driver.grab_active_posts(url)
+        driver.grab_archived_posts(url)
+
+    return driver.collect_data()
+
+
+INSTANCES = 5
+HEADLESS = True
+
+if __name__ == '__main__':
+    driver = UnituDriver(headless=HEADLESS)
     driver.login()
 
     urls = driver.get_all_board_urls()
 
-    for idx, url in enumerate(urls):
-        print(f"Processing board: {url} [{idx}/{len(urls)}]")
-        driver.grab_active_posts(url)
-        driver.grab_archived_posts(url)
+    # Divide the URLs into INSTANCES segments
+    segments = [urls[i::INSTANCES] for i in range(INSTANCES)]
 
-    driver.dump_json()
+    all_data = []
 
-    # # store the html of the page to a file
-    # with open("file.html", "w") as f:
-    #     f.write(driver.page_source)
+    with ThreadPoolExecutor(max_workers=INSTANCES) as executor:
+        futures = [executor.submit(process_board, segment) for segment in segments]
+
+        for future in as_completed(futures):
+            all_data.extend(future.result())
+
+    # Print or save the aggregated data
+    with open('aggregated_data.json', 'w') as f:
+        json.dump(all_data, f, indent=4)
+
+    print(f"Processing complete. Data saved to 'aggregated_data.json'. Len data: {len(all_data)}")
