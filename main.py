526eb6cdf0417cf57367a57ca1d49586de86fd72
DaKheera47 <shaheergoodboy@gmail.com>
2024-06-03 12:26:21 +0100

    main fixed

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from UnituDriver import UnituDriver
 
 if __name__ == '__main__':
     driver = UnituDriver(
-        headless=False
+        headless=True
     )
 
     driver.login()
 
-    # driver.grab_active_posts()
-    driver.grab_archived_posts(limit=5)
+    driver.grab_active_posts()
+    driver.grab_archived_posts()
 
-    print(driver.get_data())
+    driver.collect_data()
+
+    driver.dump_json()
 
     # # store the html of the page to a file
     # with open("file.html", "w") as f:
